c0b0df9d4e52adfdca685e256c49e676a0ab3105
irfanmaulana126 <irfanmaulana126@gmail.com>
2022-05-25 22:33:40 +0700

    remove command

--- core/urls.py
+++ core/urls.py
@@ -16,11 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-# from rest_framework import routers
-# from api import api_views as api_views
-
-# router = routers.DefaultRouter()
-# router.register(r'posts',api_views.PostsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
